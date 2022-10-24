a0bda4e6a2020536022244c3d605b77bd485632c
Count Onyu <75731806+anavjeong@users.noreply.github.com>
2022-10-24 15:33:31 +0900

    python 자료형 데이터

---
+++ python_data_type.py
@@ -0,0 +1,6 @@
+x = "python"
+print(type(x))
+
+
+
+a = 10 +10
---
+++ test.py/test.py
@@ -0,0 +1,58 @@
+# # #print('Hello World')
+
+# # print('ONyu')
+
+
+
+
+
+# # a = 'Act as \n hi'
+
+# # print(a)
+
+
+# # a = """
+# # hi
+
+
+# # there
+# # it's onyu
+# # """
+
+# # print(a)
+
+
+
+# # a = ' "Failure is simplly the opportunity to begin again" '
+# # print(a)
+# # print(len(a))
+
+# # c = "hello python!"
+
+# # print(c[0:4])
+# # print(c[:])
+
+
+# # x ='TitanicJames'
+
+# # print("title: " + x[:7])
+# # print("director: " + x[7:])
+
+
+# a = ['a', 'b', ['c','d','e']]
+# print(a[2][1])
+
+
+t =('a','a','a','b')
+print(type(t))
+
+x = {'first name':'Onyu','last name':'Jeong'} # Dict
+print(x['last name'])
+
+
+#Set  집합형 순서가 없고 , 인덱스가 없는 모음 
+# 중복을 없애 준다.
+#set() 을 사용해 만들 수 있다.
+
+y = set(['1','2','1'])
+print(y)
